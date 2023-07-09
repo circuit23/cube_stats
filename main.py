0d5eeba954a5a57862eb6a4de8b1ed45be76d1d0
circuit23 <circuit.23@gmail.com>
2023-07-09 19:31:12 -0400

    fixes regex logic to also pull out penalties adds +2 penalties, ignores DNFs better mean line fixes PEP stuff

--- main.py
+++ main.py
@@ -2,35 +2,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 import seaborn as sns
+from tinydb import TinyDB, Query
+
+# TODO: create function for storing and retrieving stats from TinyDB and graphing on that instead
+# TODO: implement the ability to import any/all cstimer exports in the directory, without creating dupes in TinyDB
 
 
-# Get WCA average (drop fastest and slowest, get mean) from any list of times
 def find_wca_avg(list_of_times):
+    """
+    Get WCA average (drop fastest and slowest, get mean) from any list of times
+    """
     truncated_list = [i for i in list_of_times if min(list_of_times) < i < max(list_of_times)]
     tlist_mean = np.mean(truncated_list)
     return tlist_mean
 
 
 def avg_over_x(list_of_times, x):
+    """
+    Get Average Over X/list slicing, using get_wca_avg method
+    """
     a_o_x = []
     for i, _ in enumerate(list_of_times):
         if i + 1 < x:
             continue
         else:
             a_o_x.append((i + 1, find_wca_avg(list_of_times[i - x + 1:i + 1])))
-
     return a_o_x
 
 
 # Read the export file into a str object
 export = open('cstimer_20230708_203358.txt', 'r').read()
-
-# Create a regex to match the actual times
-pattern = re.compile(r"\[\[0,(\d+)]")
-
-# Extract the times and convert them into floats
-times = [int(t) * 0.001 for t in pattern.findall(export)]
-
+# Create a regex to match the actual penalties (like +2 or DNF) and times
+pattern = re.compile(r"\[\[(\d+),(\d+)]")
+# Extract the penalties and times, append them to the list
+times = []
+for penalty, time in pattern.findall(export):
+    if int(penalty) == -1:  # Ignore DNFs; I just want solve stats
+        continue
+    else:
+        times.append((int(penalty) + int(time)) * 0.001)
 # Set up Seaborn stylez
 sns.set_style("darkgrid")
 
@@ -49,7 +59,7 @@ if len(times) >= 100:
 
 # Mean line
 total_mean = np.mean(times)
-plt.axhline(total_mean, color='red', linestyle='--', linewidth=3, label=f"Mean: {total_mean}")
+plt.axhline(total_mean, color='red', linestyle='--', label=f"Mean: {total_mean}")
 
 plt.legend()
 plt.show()
